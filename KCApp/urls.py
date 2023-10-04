64928db54aba07595a1265d081d6e63356283db8
lukeleech <lukeyoobee@gmail.com>
2023-10-04 22:20:59 +1300

    Basic Backend Logic added (listing forms and html templates)

--- KCApp/urls.py
+++ KCApp/urls.py
@@ -3,4 +3,11 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.LandingPage, name='landingpage'),
+    path('listings/', views.Listings, name='listings'),
+    path('newlisting/', views.NewListing, name='newlisting'),
+    path('viewlisting/', views.ViewListing, name='viewlisting'),
+    path('login/', views.login, name='login'),
+    
+]
