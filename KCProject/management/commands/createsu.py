291c576ced8b4c98a64bfadef488d34004535dae
lukeleech <lukeyoobee@gmail.com>
2023-11-13 17:13:27 +1300

    added super user command and management directory

---
+++ KCProject/management/commands/createsu.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+
+   def handle(self, *args, **options):
+        # Check if a user record with the username 'awsadmin' exists in the database
+        # If it does not exist, then create a super-user with that username,
+        # Supply an email address and a password to it
+        # So, now we have an admin user to manage our AWS RDS database
+        if not User.objects.filter(username="luke").exists():
+            #User.objects.create_superuser("adam", "adamts028@gmail.com", "AWSAdmin123")
+            User.objects.create_superuser("luke", "lukeyoobee@gmail.com", "AWSAdmin123")
